8c441e4b2ce60f3523b380bbe0bb4b6e571ae34e
Ellis Percival <flyte@failcode.co.uk>
2018-07-23 13:44:14 +0000

    0.0.1 -> 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='celery-docker-tools',
-    version='0.0.1',
+    version='0.0.2',
     author='Ellis Percival',
     author_email='celery-docker-tools_pypi@failcode.co.uk',
     description='Celery Docker tools',
