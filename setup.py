4c64c15fe1703dffc72d240b0ab64fbfb9bb0b4c
Ellis Percival <flyte@failcode.co.uk>
2018-07-23 14:43:20 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='celery-docker-tools',
+    version='0.0.1',
+    author='Ellis Percival',
+    author_email='celery-docker-tools_pypi@failcode.co.uk',
+    description='Celery Docker tools',
+    license='MIT',
+    install_requires=['docker', 'celery']
+)
